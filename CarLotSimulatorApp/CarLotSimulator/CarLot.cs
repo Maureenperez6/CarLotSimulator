b4c9a029457197a064f31feb85332aea233f08dc
Maureen Perez <maureen8694@gmail.com>
2022-01-20 15:34:58 -0500

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+
+        public CarLot()
+        {
+
+        }
+        public List<Car> ParkingLot { get; set; } = new List<Car>(); // Made into a Property 
+        //List<Car> ParkingLot = new List<car>(); --> Standard way
+    }
+}
+
+
+// FOR ***BONUS X 2***
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,9 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLot = new CarLot(); // for BONUZ X 2 only needed for BOUNS X 2 !!! --> Instantiating a new object of type CarLot
+
+            
             //TODO
 
             //Create a seperate class file called Car
@@ -22,12 +25,73 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            //**Dot Notation**
+            var toyota = new Car(); // instantiating a new car
+            toyota.Year = 2021;
+            toyota.Make = "Toyota";
+            toyota.Model = "4Runner";
+            toyota.EngineNoise = "Vroom, Vroom";
+            toyota.HonkNoise = "Beep, Beep";
+            toyota.IsDrivable = true;
+
+            carLot.ParkingLot.Add(toyota); // for ***BONUS X 2*** only needed for BONUS X 2 !!!
+
+            //**Object Initialize Syntax**
+            var honda = new Car()
+            {
+                Year = 2001,
+                Make = "Honda",
+                Model =  "Accord",
+                EngineNoise = "Rattle, Rattle...",
+                HonkNoise = "Beep,Beep",
+                IsDrivable = false
+
+            };
+            carLot.ParkingLot.Add(honda); // for ***BONUS X 2*** only needed for BONUS X 2 !!!
+
+            //**Custom Constructor**
+            var infiniti = new Car(2014, "Infiniti", "Q50", "Vroom!", "BEEEP!", true);
+
+            carLot.ParkingLot.Add(infiniti); // for *** BONUS X 2*** only needed for BONUS X 2 !!!
+
+           
+            Console.WriteLine("Toyota");
+            toyota.MakeEngineNoise(toyota.EngineNoise);
+            toyota.MakeHonkeNoise(toyota.HonkNoise);
+            Console.WriteLine();
+
+            Console.WriteLine("Honda");
+            honda.MakeEngineNoise(honda.EngineNoise);
+            honda.MakeHonkeNoise(honda.HonkNoise);
+            Console.WriteLine();
+
+            Console.WriteLine("Infiniti");
+            infiniti.MakeEngineNoise(infiniti.EngineNoise);
+            infiniti.MakeHonkeNoise(infiniti.HonkNoise);
+            Console.WriteLine();
+
+
+
+
+
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+           
+            Console.WriteLine("-----------------");
+
+            foreach (var car in carLot.ParkingLot) // ITERATING THROUGH THE LIST
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+                Console.WriteLine();
+                car.MakeEngineNoise(car.EngineNoise);
+                car.MakeHonkeNoise(car.HonkNoise);
+                
+                // can also add /n to end before " to make a space.
+            }
         }
     }
 }
